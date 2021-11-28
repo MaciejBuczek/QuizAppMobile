c2b4c2ff6fbf611be3d6e63bb9581042c560d328
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-11-28 18:06:30 +0100

    http client provider implemented

--- QuizAppMobile/Services/Implementations/UserAPIService.cs
+++ QuizAppMobile/Services/Implementations/UserAPIService.cs
@@ -1,20 +1,22 @@
-﻿using Newtonsoft.Json;
-using QuizAppMobile.Configs;
+﻿using QuizAppMobile.Configs;
 using QuizAppMobile.Models.API;
 using QuizAppMobile.Services.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace QuizAppMobile.Services.Implementations
 {
     public class UserAPIService : IUserService
     {
+        private readonly IHttpClientProvider _httpClientProvider;
 
-        private readonly HttpClient _httpClient = new HttpClient();
+        public UserAPIService()
+        {
+            _httpClientProvider = DependencyService.Resolve<IHttpClientProvider>();
+        }
 
         public Task<bool> LoginAsync(string username, string email)
         {
@@ -29,18 +31,10 @@ namespace QuizAppMobile.Services.Implementations
                 Email = email,
                 Password = password
             };
-
-            var handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
-            {
-                if (cert.Issuer.Equals("CN=localhost"))
-                    return true;
-                return errors == System.Net.Security.SslPolicyErrors.None;
-            };
-            var client = new HttpClient(handler);
-
             var url = APIConfigs.BaseURL + APIConfigs.RegisterURL;
             HttpContent httpContent = JsonContent.Create(request);
+
+            var client = _httpClientProvider.GetClient();
 
             var response = await client.PostAsync(url, httpContent);
             return response.IsSuccessStatusCode;
---
+++ QuizAppMobile/Services/Interfaces/IHttpClientProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace QuizAppMobile.Services.Interfaces
+{
+    public interface IHttpClientProvider
+    {
+        HttpClient GetClient();
+    }
+}

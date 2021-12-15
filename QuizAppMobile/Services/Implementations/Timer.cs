dcd69312de98d9f1d18b894d9f72e212bd91671a
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-12-15 21:38:20 +0100

    quiz timer testing

---
+++ QuizAppMobile/Services/Implementations/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuizAppMobile.Services.Implementations
+{
+    public class Timer : CancellationTokenSource
+    {
+        private readonly Action _finishAction;
+        private int duration;
+
+        public string TimerFormatted { 
+            get 
+            {
+                int minutes = duration / 60;
+                int seconds = duration % 60;
+                string minutesString = minutes < 10 ? $"0{minutes}" : minutes.ToString();
+                string secondsString = seconds < 10 ? $"0{seconds}" : seconds.ToString();
+                return $"{minutesString}:{secondsString}";
+            }
+        }
+
+        public Timer(Action finishingAction)
+        {
+            _finishAction = finishingAction;
+        }
+
+
+        public void Start(int totalDuration)
+        {
+            duration = 0;
+            
+            Task.Run(() =>
+            {
+                while(totalDuration > duration && !IsCancellationRequested)
+                {
+                    Task.Delay(1000, Token);
+                }
+                _finishAction();             
+            });
+        }
+
+        public void Stop()
+        {
+            Cancel();
+        }
+    }
+}
--- QuizAppMobile/ViewModels/QuizVM.cs
+++ QuizAppMobile/ViewModels/QuizVM.cs
@@ -1,26 +1,40 @@
 ﻿using QuizAppMobile.Models;
+using QuizAppMobile.Services.Implementations;
+using QuizAppMobile.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace QuizAppMobile.ViewModels
 {
     class QuizVM : BindableObject
     {
+        private readonly IMessageService _messageService;
+
         public string LobbyCode { get; set; }
 
         public string Title { get; set; }
 
         public string Question { get; set; }
 
+        public Timer Timer { get; }
+
         public ObservableCollection<Answer> Answers {get; set;}
 
         public ObservableCollection<UserScore> Users { get; set; }
 
+        public ICommand Test { get; set; }
+
         public QuizVM()
         {
+            Timer = new Timer(async () => await SubmitAnswer());
+            _messageService = DependencyService.Resolve<IMessageService>();
+            Test = new Command(TestAct);
+
             Answers = new ObservableCollection<Answer>()
             {
                 new Answer
@@ -52,6 +66,16 @@ namespace QuizAppMobile.ViewModels
                     Username = "Rosomak"
                 }
             };
+        }
+
+        public void TestAct()
+        {
+            Timer.Start(10);
+        }
+
+        public async Task SubmitAnswer()
+        {
+            await _messageService.DisplaySuccessMessage("test");
         }
     }
 }

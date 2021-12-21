8f0149fdb407fff48cdf3e4fc63eebb0c7c04088
MaciejBuczek <49284699+MaciejBuczek@users.noreply.github.com>
2021-12-21 16:56:03 +0100

    Summary Page display stats fixed

--- QuizAppMobile/ViewModels/SummaryVM.cs
+++ QuizAppMobile/ViewModels/SummaryVM.cs
@@ -11,52 +11,42 @@ using Xamarin.Forms;
 
 namespace QuizAppMobile.ViewModels
 {
-    public class SummaryVM
+    public class SummaryVM : BindableObject
     {
         private readonly ISummaryService _summaryService;
         private readonly IMessageService _messageService;
 
+        private double _score;
+
+        private string _place;
+
         public string LobbyCode { get; set; }
 
-        public ObservableCollection<UserScorePlace> UsersScores { get; set; } = new ObservableCollection<UserScorePlace>();
+        public ObservableCollection<UserScorePlace> UsersScoresPlaces { get; set; } = new ObservableCollection<UserScorePlace>();
 
         public double Score
         {
+            set
+            {
+                _score = value;
+                OnPropertyChanged(nameof(Score));
+            }
             get
             {
-                if (UsersScores == null || UsersScores.Count == 0)
-                    return 0;
-
-                var username = (string) Application.Current.Properties[Properties.Username];
-                return UsersScores.Where(us => us.Username == username).FirstOrDefault().Score;
+                return _score;
             }
         }
 
         public string Place
         {
+            set
+            {
+                _place = value;
+                OnPropertyChanged(nameof(Place));
+            }
             get
             {
-                if (UsersScores == null || UsersScores.Count == 0)
-                    return string.Empty;
-
-                var username = (string)Application.Current.Properties[Properties.Username];
-                int place = 0;
-                for(int i = UsersScores.Count - 1; i>=0; i--)
-                {
-                    if (UsersScores[i].Username == username)
-                        place = i;
-                }
-                switch (place)
-                {
-                    case 1:
-                        return "1st";
-                    case 2:
-                        return "2nd";
-                    case 3:
-                        return "3rd";
-                    default:
-                        return $"{place}th";
-                }
+                return _place;
             }
         }
 
@@ -72,22 +62,58 @@ namespace QuizAppMobile.ViewModels
 
             if (response.Succeded)
             {
-                for(int i = 1; i <= response.UsersScores.Count; i++)
+                Score = GetScore(response.UsersScores);
+                Place = GetPlace(response.UsersScores);
+
+                for (int i = 1; i <= response.UsersScores.Count; i++)
                 {
-                    var userScorePlace = new UserScorePlace()
+                    UsersScoresPlaces.Add(new UserScorePlace()
                     {
-                        Username = response.UsersScores[i].Username,
-                        Score = response.UsersScores[i].Score,
+                        Username = response.UsersScores[i - 1].Username,
+                        Score = response.UsersScores[i - 1].Score,
                         Place = i
-                    };
-                    UsersScores.Add(userScorePlace);
+                    });
                 }
             }
             else
             {
                 await _messageService.DisplayErrorMessage("Lost connection to the server");
                 await Application.Current.MainPage.Navigation.PopAsync();
+            }   
+        }
+
+        private string GetPlace(List<Models.SignalR.UserScore> userScores)
+        {
+            if (userScores == null || userScores.Count == 0)
+                return string.Empty;
+
+            var username = (string)Application.Current.Properties[Properties.Username];
+            int place = 0;
+            for (int i = userScores.Count - 1; i >= 0; i--)
+            {
+                if (userScores[i].Username == username)
+                    place = i;
             }
+            switch (place)
+            {
+                case 1:
+                    return "1st";
+                case 2:
+                    return "2nd";
+                case 3:
+                    return "3rd";
+                default:
+                    return $"{place}th";
+            }
+        }
+
+        private double GetScore(List<Models.SignalR.UserScore> userScores)
+        {
+            if (userScores == null || userScores.Count == 0)
+                return 0;
+
+            var username = (string)Application.Current.Properties[Properties.Username];
+            return userScores.Where(us => us.Username == username).FirstOrDefault().Score;
         }
     }
 }
